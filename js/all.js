12063a1c0ec91c006c6291e0282e855333a1670a
victorscher <victorscher92@gmail.com>
2020-02-10 10:49:49 -0300

    .

--- js/all.js
+++ js/all.js
@@ -209,6 +209,7 @@ function swipedetect(el, callback){
   var touchsurface = el,
   swipedir,
   startX,
+  startY,
   distX,
   distY,
   threshold = 150, //required min distance traveled to be considered swipe
@@ -223,6 +224,7 @@ function swipedetect(el, callback){
       swipedir = 'none'
       dist = 0
       startX = touchobj.pageX
+      startY = touchobj.pageY
       startTime = new Date().getTime() // record time when finger first makes contact with surface
       e.preventDefault()
   }, false)
@@ -234,10 +236,14 @@ function swipedetect(el, callback){
   touchsurface.addEventListener('touchend', function(e){
       var touchobj = e.changedTouches[0]
       distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+      distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
       elapsedTime = new Date().getTime() - startTime // get time elapsed
       if (elapsedTime <= allowedTime){ // first condition for awipe met
           if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
               swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+          }
+          else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+              swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
           }
       }
       handleswipe(swipedir)
